f6aa96473abaf902db1d300da66d02b34d6176c2
eipc16 <pprzemek.312@gmail.com>
2019-05-27 10:52:57 +0200

    Working Rest Service

---
+++ WcfRestService/IRestService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestService
+{
+    [ServiceContract]
+    public interface IRestService
+    {
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "/books",
+            ResponseFormat = WebMessageFormat.Json)]
+        List<BookEntity> getAll();
+
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "/books/xml",
+            ResponseFormat = WebMessageFormat.Xml)]
+        List<BookEntity> getAllXML();
+
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "/books/{id}",
+            ResponseFormat = WebMessageFormat.Json)]
+        BookEntity getById(string id);
+
+        [OperationContract]
+        [WebGet(
+            UriTemplate = "/books/{id}/xml",
+            ResponseFormat = WebMessageFormat.Xml)]
+        BookEntity getByIdXML(string id);
+
+        [OperationContract]
+        [WebInvoke(
+            UriTemplate = "/books/{id}",
+            Method = "PUT",
+            ResponseFormat = WebMessageFormat.Json)]
+        string update(string id, BookEntity book);
+
+        [OperationContract]
+        [WebInvoke(
+        UriTemplate = "/books/{id}/xml",
+        Method = "PUT",
+        ResponseFormat = WebMessageFormat.Xml)]
+        string updateXML(string id, BookEntity book);
+    }
+
+
+    [DataContract(IsReference = false)]
+    public class BookEntity
+    {
+        [DataMember]
+        public long BookID;
+
+        [DataMember]
+        public string BookTitle;
+
+        [DataMember]
+        public int PublishYear;
+
+        public BookEntity(long BookID, string BookTitle, int PublishYear)
+        {
+            this.BookID = BookID;
+            this.BookTitle = BookTitle;
+            this.PublishYear = PublishYear;
+        }
+    }
+}
---
+++ WcfRestService/RestService.svc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfRestService
+{
+    [ServiceBehavior(InstanceContextMode =InstanceContextMode.Single)]
+    public class RestService : IRestService
+    {
+        private static List<BookEntity> books;
+
+        public RestService()
+        {
+            books = new List<BookEntity>()
+            {
+                new BookEntity(1, "Książka 1", 2017),
+                new BookEntity(2, "Książka 2", 2018),
+                new BookEntity(3, "Książka 3", 2019)
+            };
+        }
+
+        public List<BookEntity> getAll()
+        {
+            return books;
+        }
+
+        public List<BookEntity> getAllXML()
+        {
+            return getAll();
+        }
+
+        public BookEntity getById(string id)
+        {
+            long BookID = long.Parse(id);
+            return books.Find(b => b.BookID == BookID);
+        }
+
+        public BookEntity getByIdXML(string id)
+        {
+            return getById(id);
+        }
+
+        public string update(string id, BookEntity book)
+        {
+            long BookID = long.Parse(id);
+
+            if (book == null)
+                throw new ArgumentNullException("Invalid argument: book");
+
+            int bookIndex = books.FindIndex(b => b.BookID == BookID);
+
+            if (bookIndex < 0)
+                return "Could not find book with BookID=" + BookID;
+
+            books.RemoveAt(bookIndex);
+            books.Add(book);
+
+            return "Updated book with BookID=" + BookID;
+        }
+
+        public string updateXML(string id, BookEntity book)
+        {
+            return update(id, book);
+        }
+    }
+}

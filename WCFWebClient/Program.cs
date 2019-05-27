6293a42b42e790c8d4b68a0bb7d86de31ceaa231
eipc16 <pprzemek.312@gmail.com>
2019-05-27 09:34:20 +0200

    update

--- WCFWebClient/Program.cs
+++ WCFWebClient/Program.cs
@@ -12,13 +12,16 @@ namespace WCFWebClient
         {
             Service1Client client = new Service1Client("BasicHttpBinding_IService1");
 
+            client.Open();
+
             for(int i = 0; i < 25; i += 5)
             {
                 showFactorial(client, i);
             }
-
             Console.WriteLine("Press <ENTER> to exit!");
             Console.ReadKey();
+
+            client.Close();
         }
 
         static void showFactorial(Service1Client client, long n)
